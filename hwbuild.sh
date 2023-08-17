bc3c20418b53887e84cb517a9f9c7a79cc674887
sciancepi0@gmail.com <sciancepi0@gmail.com>
2023-08-17 15:59:54 +0100

    Fixed hwbuild.sh

--- hwbuild.sh
+++ hwbuild.sh
@@ -7,29 +7,8 @@ echo "#ifndef BUILD_TYPE_H
 
 #endif" > core/build_type.h
 echo "Writing makefile..."
-echo "TARGET ?= picos
-SRC_DIRS ?= ./src ./core ./os ./apps
-
-SRCS := $(shell find $(SRC_DIRS) -name *.cpp -or -name *.c -or -name *.s)
-OBJS := $(addsuffix .o,$(basename $(SRCS)))
-DEPS := $(OBJS:.o=.d)
-
-INC_DIRS := $(shell find $(SRC_DIRS) -type d)
-INC_FLAGS := $(addprefix -I,$(INC_DIRS))
-
-LDFLAGS = -lwiringPi
-CPPFLAGS ?= $(INC_FLAGS) -MMD -MP -O3 -Wall -Wextra -Werror -pedantic
-CC = clang
-
-$(TARGET): $(OBJS)
-	$(CC) $(LDFLAGS) $(OBJS) -o $@ $(LOADLIBES) $(LDLIBS)
-
-.PHONY: clean
-clean:
-	$(RM) $(TARGET) $(OBJS) $(DEPS)
-
-test:
-	./$(TARGET)
-
--include $(DEPS)" > Makefile
+echo "Renaming Makefile.old..."
+mv Makefile Makefile.old1
+mv Makefile.old Makefile
+mv Makefile.old1 Makefile.old
 echo "Done configuring environment for HARDWARE_BUILD"
